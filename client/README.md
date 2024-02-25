4fbcf9267ba15971e6dde3f90f611388519a7c4a
Cauã Rinaldi <cauarinaldi451@gmail.com>
2024-02-25 09:13:39 -0300

    Fix form

--- client/README.md
+++
@@ -1,5 +0,0 @@
-# Requirements
-- Rustup
-- Node
-- Envoy: https://www.envoyproxy.io/docs/envoy/latest/start/install#install-envoy-on-ubuntu-linux
-- grpc-web protoc plugin: https://github.com/grpc/grpc-web?tab=readme-ov-file#code-generator-plugin
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,42 @@
 import {EchoRequest, EchoServiceClient} from "./grpc-api/echo.ts";
+import React from "react";
 
 
-function App() {
+export const App: React.FC = () => {
+  const [message, setMessage] = React.useState("")
+  const [response, setResponse] = React.useState("")
+  const [isLoading, setIsLoading] = React.useState(false)
 
   return (
-    <>
-      Hey
-        <button onClick={async () => {
-          const echoService = new EchoServiceClient('http://localhost:50051', undefined, {
-            // suppressCorsPreflight: true
-          });
-          const req = new EchoRequest()
-          req.message=("Message hey")
-          const res = await echoService.Echo(req, {})
-          console.log(res.message)
-        }}>Click</button>
-    </>
+    <div>
+      <h3>
+        Server echo
+      </h3>
+      <form onSubmit={async (e) => {
+        e.preventDefault()
+
+        setIsLoading(true)
+        const echoService = new EchoServiceClient('http://localhost:50051');
+        const req = new EchoRequest()
+        req.message = message
+        const res = await echoService.Echo(req, {})
+        console.log(res.message)
+        setResponse(res.message)
+        setIsLoading(false)
+      }}>
+        <div style={{ marginBottom: '0.5rem' }}>
+          <label htmlFor='messageInput'>Message:</label>
+          <br/>
+          <input id='messageInput' value={message} onChange={(event) => setMessage(event.currentTarget.value)}/>
+        </div>
+
+        <div>
+          <button type='submit'>Send echo request</button>
+        </div>
+        <p>
+          {isLoading ? 'Loading...' : `Response: "${response}"`}
+        </p>
+      </form>
+    </div>
   )
 }
-
-export default App
