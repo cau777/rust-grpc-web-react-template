7e5d82251569197be7219c34e2503fb796435745
Cauã Rinaldi <cauarinaldi451@gmail.com>
2024-02-24 23:10:52 -0300

    Fix cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,10 +7,12 @@ function App() {
     <>
       Hey
         <button onClick={async () => {
-          const echoService = new EchoServiceClient('http://localhost:9090');
+          const echoService = new EchoServiceClient('http://localhost:50051', undefined, {
+            // suppressCorsPreflight: true
+          });
           const req = new EchoRequest()
           req.message=("Message hey")
-          const res = await echoService.Echo(req, null)
+          const res = await echoService.Echo(req, {})
           console.log(res.message)
         }}>Click</button>
     </>
--- grpc-backend/src/main.rs
+++ grpc-backend/src/main.rs
@@ -1,17 +1,52 @@
+use std::fmt::Debug;
+use std::time::Duration;
+use tonic::codegen::http::{Request, Response};
 use tonic::transport::Server;
+use tower_http::trace::{OnFailure, OnRequest, OnResponse, TraceLayer};
+use tracing::Span;
 use crate::services::echo_service;
 
+
 mod services;
+
+#[derive(Clone)]
+struct Logger;
+
+impl<B: Debug> OnResponse<B> for Logger {
+    fn on_response(self, response: &Response<B>, latency: Duration, span: &Span) {
+        println!("Response ({:?}):", response.status());
+        println!("Response ({:?}): {:?}", response.status().canonical_reason(), response.status().as_str());
+        println!("Headers: {:?}", response.headers());
+        println!("Body: {:?}", response.body());
+        println!();
+    }
+}
+
+impl<B> OnFailure<B> for Logger {
+    fn on_failure(&mut self, failure_classification: B, latency: Duration, span: &Span) {
+        // TODO: better logging
+        print!("Failure")
+    }
+}
+
+impl<B> OnRequest<B> for Logger {
+    fn on_request(&mut self, request: &Request<B>, span: &Span) {
+        println!("Request");
+        println!("Headers: {:?}", request.headers());
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let addr = "[::1]:50051".parse().unwrap();
-
     println!("Server listening on {}", addr);
 
+
     Server::builder()
+        .accept_http1(true)
+        .layer(TraceLayer::new_for_grpc().on_request(Logger).on_response(Logger).on_failure(Logger))
         .add_service(
-            echo_service::messages::echo_service_server::EchoServiceServer::new(echo_service::EchoServiceImpl)
+            tonic_web::enable(echo_service::messages::echo_service_server::EchoServiceServer::new(echo_service::EchoServiceImpl))
         )
         .serve(addr)
         .await?;
